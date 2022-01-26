557b77605ef06d2fe676eee9aa302dbd5addda4c
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2022-01-26 22:18:00 +0530

    Add files via upload

---
+++ 2048.py
@@ -0,0 +1,231 @@
+from tkinter import *
+import random
+import colour as c
+
+
+class Game(Frame):
+    def __init__(self):
+        Frame.__init__(self)
+        self.grid()
+        self.master.title("2048")
+        self.config(bg="#14213d")
+        self.master.resizable(False, False)
+        self.main_grid = Frame(self, bg=c.GRID_COLOR, bd=3, width=400, height=400)
+        self.main_grid.grid(pady=(100, 0))  # creating the grid
+        self.make_GUI()
+        self.start_game()
+
+        self.master.bind("<Left>", self.left)
+        self.master.bind("<Right>", self.right)
+        self.master.bind("<Up>", self.up)
+        self.master.bind("<Down>", self.down)
+
+        self.mainloop()
+
+    def make_GUI(self):
+        self.cells = []
+        for i in range(4):
+            row = []
+            for j in range(4):
+                cell_frame = Frame(
+                    self.main_grid, bg=c.EMPTY_CELL_COLOR, width=100, height=100
+                )
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
+                cell_number = Label(self.main_grid, bg=c.EMPTY_CELL_COLOR)
+                cell_number.grid(row=i, column=j)
+                cell_data = {"frame": cell_frame, "number": cell_number}
+                row.append(cell_data)
+            self.cells.append(row)
+
+        score_frame = Frame(self, bg="#14213d")
+        score_frame.place(relx=0.5, y=40, anchor="center")
+        Label(
+            score_frame,
+            text="SCORE",
+            bg="#14213d",
+            fg=c.SCORE_FG,
+            font=c.SCORE_LABEL_FONT,
+        ).grid(row=0)
+        self.score_label = Label(
+            score_frame, text="0", bg="#14213d", fg=c.SCORE_FG, font=c.SCORE_FONT
+        )
+        self.score_label.grid(row=1)
+
+    def start_game(self):
+        self.matrix = [[0] * 4 for _ in range(4)]
+        row = random.randint(0, 3)
+        col = random.randint(0, 3)
+        self.matrix[row][col] = 2
+        self.cells[row][col]["frame"].configure(bg=c.CELL_COLORS[2])
+        self.cells[row][col]["number"].configure(
+            bg=c.CELL_COLORS[2],
+            fg=c.CELL_NUMBER_COLORS[2],
+            font=c.CELL_NUMBER_FONTS[2],
+            text="2",
+        )
+
+        while self.matrix[row][col] != 0:
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+
+        self.matrix[row][col] = 2
+        self.cells[row][col]["frame"].configure(bg=c.CELL_COLORS[2])
+        self.cells[row][col]["number"].configure(
+            bg=c.CELL_COLORS[2],
+            fg=c.CELL_NUMBER_COLORS[2],
+            font=c.CELL_NUMBER_FONTS[2],
+            text="2",
+        )
+
+        self.score = 0
+
+    def stack(self):
+        new_matrix = [[0] * 4 for _ in range(4)]
+        for i in range(4):
+            fill_position = 0
+            for j in range(4):
+                if self.matrix[i][j] != 0:
+                    new_matrix[i][fill_position] = self.matrix[i][j]
+                    fill_position += 1
+        self.matrix = new_matrix
+
+    def combine(self):
+        for i in range(4):
+            for j in range(3):
+                if (
+                    self.matrix[i][j] != 0
+                    and self.matrix[i][j] == self.matrix[i][j + 1]
+                ):
+                    self.matrix[i][j] *= 2
+                    self.matrix[i][j + 1] = 0
+                    self.score += self.matrix[i][j]
+
+    def reverse(self):
+        new_matrix = []
+        for i in range(4):
+            new_matrix.append([])
+            for j in range(4):
+                new_matrix[i].append(self.matrix[i][3 - j])
+        self.matrix = new_matrix
+
+    def transpose(self):
+        new_matrix = [[0] * 4 for _ in range(4)]
+        for i in range(4):
+            for j in range(4):
+                new_matrix[i][j] = self.matrix[j][i]
+        self.matrix = new_matrix
+
+    def add_new_tile(self):
+        if any(0 in row for row in self.matrix):
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+        while self.matrix[row][col] != 0:
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+        self.matrix[row][col] = random.choice([2, 4])
+
+    def update_GUI(self):
+        for i in range(4):
+            for j in range(4):
+                cell_value = self.matrix[i][j]
+                if cell_value == 0:
+                    self.cells[i][j]["frame"].configure(bg=c.EMPTY_CELL_COLOR)
+                    self.cells[i][j]["number"].configure(bg=c.EMPTY_CELL_COLOR, text="")
+                else:
+                    self.cells[i][j]["frame"].configure(bg=c.CELL_COLORS[cell_value])
+                    self.cells[i][j]["number"].configure(
+                        bg=c.CELL_COLORS[cell_value],
+                        fg=c.CELL_NUMBER_COLORS[cell_value],
+                        font=c.CELL_NUMBER_FONTS[cell_value],
+                        text=str(cell_value),
+                    )
+        self.score_label.configure(text=self.score)
+        self.update_idletasks()
+
+    def left(self, event):
+        self.stack()
+        self.combine()
+        self.stack()
+        self.add_new_tile()
+        self.update_GUI()
+        self.game_over()
+
+    def right(self, event):
+        self.reverse()
+        self.stack()
+        self.combine()
+        self.stack()
+        self.reverse()
+        self.add_new_tile()
+        self.update_GUI()
+        self.game_over()
+
+    def up(self, event):
+        self.transpose()
+        self.stack()
+        self.combine()
+        self.stack()
+        self.transpose()
+        self.add_new_tile()
+        self.update_GUI()
+        self.game_over()
+
+    def down(self, event):
+        self.transpose()
+        self.reverse()
+        self.stack()
+        self.combine()
+        self.stack()
+        self.reverse()
+        self.transpose()
+        self.add_new_tile()
+        self.update_GUI()
+        self.game_over()
+
+    def horizontal_move_exists(self):
+        for i in range(4):
+            for j in range(3):
+                if self.matrix[i][j] == self.matrix[i][j + 1]:
+                    return True
+        return False
+
+    def vertical_move_exists(self):
+        for i in range(3):
+            for j in range(4):
+                if self.matrix[i][j] == self.matrix[i + 1][j]:
+                    return True
+        return False
+
+    def game_over(self):
+        if any(2048 in row for row in self.matrix):
+            game_over_frame = Frame(self.main_grid, borderwidth=2)
+            game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
+            Label(
+                game_over_frame,
+                text="You Win!",
+                bg=c.WINNER_BG,
+                fg=c.GAME_OVER_FONT_COLOR,
+                font=c.GAME_OVER_FONT_COLOR,
+            ).pack()
+        elif (
+            not any(0 in row for row in self.matrix)
+            and not self.horizontal_move_exists()
+            and not self.vertical_move_exists()
+        ):
+            game_over_frame = Frame(self.main_grid, borderwidth=2)
+            game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
+            Label(
+                game_over_frame,
+                text="Game Over!",
+                bg=c.LOSER_BG,
+                fg=c.GAME_OVER_FONT_COLOR,
+                font=c.GAME_OVER_FONT,
+            ).pack()
+
+
+def main():
+    Game()
+
+
+if __name__ == "__main__":
+    main()
---
+++ colour.py
@@ -0,0 +1,51 @@
+GRID_COLOR = "#0c0f0a"
+EMPTY_CELL_COLOR = "#7A0000"  # or 218380
+SCORE_LABEL_FONT = ("Aerial", 20, "bold")
+SCORE_FONT = ("Helvetica", 32, "bold")
+GAME_OVER_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOR = "#fcf300"
+WINNER_BG = "#14213d"
+LOSER_BG = "#14213d"
+SCORE_FG = "#fcf300"
+
+CELL_COLORS = {
+    2: "#7209b7",
+    4: "#f72585",
+    8: "#f5b700",
+    16: "#89fc00",
+    32: "#2a9d8f",
+    64: "#ff002b",
+    128: "#b6174b",
+    256: "#13d1ff",
+    512: "#04e762",
+    1024: "#f77f00",
+    2048: "#ff0000"
+}
+
+CELL_NUMBER_COLORS = {
+    2: "#ffffff",
+    4: "#ffffff",
+    8: "#ffffff",
+    16: "#ffffff",
+    32: "#ffffff",
+    64: "#ffffff",
+    128: "#ffffff",
+    256: "#ffffff",
+    512: "#ffffff",
+    1024: "#ffffff",
+    2048: "#ffffff"
+}
+
+CELL_NUMBER_FONTS = {
+    2: ("Helvetica", 50, "bold"),
+    4: ("Helvetica", 50, "bold"),
+    8: ("Helvetica", 50, "bold"),
+    16: ("Helvetica", 45, "bold"),
+    32: ("Helvetica", 45, "bold"),
+    64: ("Helvetica", 45, "bold"),
+    128: ("Helvetica", 40, "bold"),
+    256: ("Helvetica", 40, "bold"),
+    512: ("Helvetica", 40, "bold"),
+    1024: ("Helvetica", 35, "bold"),
+    2048: ("Helvetica", 35, "bold")
+}
